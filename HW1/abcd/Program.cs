d55f125d44cb290d583797d948319bc22c403afb
ZiyaAbasov <ziyaabasov28@gmail.com>
2024-05-20 09:39:33 +0400

    added file

---
+++ HW1/abcd/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace abcd
+{
+    internal class Program 
+    { 
+
+        static void Main(string[] args)
+        {
+            int sumofnumber = SUM(15); 
+
+
+            Console.WriteLine(sumofnumber);
+        }
+
+        static int SUM(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+
+}
---
+++ Hw3/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "HelloWorld";
+            char[] CapitalLetter = ReturnChars(name);
+
+            Console.WriteLine(CapitalLetter);
+        }
+
+        static char[] ReturnChars(string name)
+        {
+           
+            char[] results = new char[name.Length];
+            int index = 0;
+            foreach(char c in name) 
+            {
+                if (c >= 'A' && c <= 'Z') 
+                {
+
+                    results[index] = c;
+                    index ++;
+                }
+            } return results;
+
+        }
+    }
+
+
+}
